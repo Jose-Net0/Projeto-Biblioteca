4376fef4edc931fe8341a25e60006e27f40b8e85
Jose Antonio <33164350+Jose-Net0@users.noreply.github.com>
2026-05-15 23:53:56 +0000

    retirando importes desnecessarios

--- src/main/java/org/iftm/modelo_api_rest/ModeloApiRestApplication.java
+++ src/main/java/org/iftm/modelo_api_rest/ModeloApiRestApplication.java
@@ -2,9 +2,6 @@ package org.iftm.modelo_api_rest;
 
 import org.iftm.modelo_api_rest.entities.Usuario;
 
-import java.time.Instant;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,10 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 public class ModeloApiRestApplication implements CommandLineRunner {
